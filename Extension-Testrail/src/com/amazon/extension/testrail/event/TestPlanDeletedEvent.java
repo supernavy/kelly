1a04c2d01aea2d5015098bba4dad90e67306ce88
lihaijun <supernavy@sina.com>
2015-03-13 17:54:43 +0800

    publish some domain events

--- Extension-Testrail/src/com/amazon/extension/testrail/event/TestPlanDeletedEvent.java
+++ Extension-Testrail/src/com/amazon/extension/testrail/event/TestPlanDeletedEvent.java
@@ -2,18 +2,13 @@ package com.amazon.extension.testrail.event;
 
 public class TestPlanDeletedEvent implements TestrailEvent
 {
-    Long projectId;
     Long testPlanId;
     
-    public TestPlanDeletedEvent(Long projectId, Long testPlanId)
+    public TestPlanDeletedEvent(Long testPlanId)
     {
-        this.projectId = projectId;
         this.testPlanId = testPlanId;
     }
-    public Long getProjectId()
-    {
-        return projectId;
-    }
+
     public Long getTestPlanId()
     {
         return testPlanId;
